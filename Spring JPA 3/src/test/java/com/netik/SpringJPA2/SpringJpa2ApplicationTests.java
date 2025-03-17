8faaffc53809fad1b336dac372f9f4e3562378d4
netik-kohli-ttn <netik.kohli@tothenew.com>
2025-03-17 16:22:28 +0530

    Update SpringJpa2ApplicationTests.java

--- Spring JPA 3/src/test/java/com/netik/SpringJPA2/SpringJpa2ApplicationTests.java
+++ Spring JPA 3/src/test/java/com/netik/SpringJPA2/SpringJpa2ApplicationTests.java
@@ -15,28 +15,4 @@ class SpringJpa2ApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void testAnswer1() {
-        Employee emp1 = new Employee();
-        emp1.setFirstName("Netik");
-        emp1.setLastName("Kohli");
-        emp1.setSalary(1800000.0);
-        emp1.setAge(23);
-
-        Employee emp2 = new Employee();
-        emp2.setFirstName("Sumit");
-        emp2.setLastName("Gupta");
-        emp2.setSalary(500000.0);
-        emp2.setAge(32);
-        employeeRepository.save(emp1);
-        employeeRepository.save(emp2);
-
-        List<Object[]> results = employeeRepository.getEmployeesAboveAverageSalary();
-        for (Object[] row : results) {
-            System.out.println("First Name: " + row[0] + ", Last Name: " + row[1]);
-        }
-    }
-
-    @Test
 }
