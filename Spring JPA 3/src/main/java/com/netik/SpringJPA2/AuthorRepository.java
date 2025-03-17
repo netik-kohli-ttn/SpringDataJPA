83cbf1dcc19ef05eff4ffa111da193deb5eafa93
netik-kohli-ttn <netik.kohli@tothenew.com>
2025-03-17 16:21:45 +0530

    Delete Spring JPA 3/src/main/java/com/netik/SpringJPA2 directory

--- Spring JPA 3/src/main/java/com/netik/SpringJPA2/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.netik.SpringJPA3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- Spring JPA 3/src/main/java/com/netik/SpringJPA2/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.netik.SpringJPA3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
